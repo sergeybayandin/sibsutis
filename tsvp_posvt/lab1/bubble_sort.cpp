8b5e2a6d996d5e5d78e2c3e78a84f4d6db525f59
art0fprogramm1ng <serezha-bayandin@mail.ru>
2021-09-01 16:01:57 +0000

    made bubble sort programm

---
+++ tsvp_posvt/lab1/bubble_sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+
+template <class BidirectionalIterator, class Compare>
+std::pair<std::size_t, std::size_t>
+bubble_sort(BidirectionalIterator first, BidirectionalIterator last, Compare comp)
+{
+	std::size_t m {}, n {};
+	if (first == last)
+		return {m, n};
+	while (first != --last)
+		for (auto _first {first}; _first != last; ++_first)
+			if ((++m, comp(*std::next(_first), *_first)))
+				(n += 3, std::iter_swap(std::next(_first), _first));
+	return {m, n};
+}
+
+int main()
+{
+	std::string s; std::getline(std::cin, s);
+	std::istringstream is {s};
+	std::istream_iterator<int> begin {is}, end;
+	std::vector<int> v(begin, end);
+	const auto [m, n] {bubble_sort(std::begin(v), std::end(v), std::less<int>{})};
+	for (auto val : v)
+		std::cout << val << '\t';
+	std::cout << '\n';
+	return EXIT_SUCCESS;
+}
