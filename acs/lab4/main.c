f47d08eb952585079c463d65051960bfa7fd2ab9
art0fprogramm1ng <serezha-bayandin@mail.ru>
2021-10-30 06:11:14 +0000

    impl dgemm_o3_sse

--- acs/lab4/main.c
+++ acs/lab4/main.c
@@ -3,6 +3,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <mmintrin.h> /* MMX */
+#include <xmmintrin.h> /* SSE, нужен также mmintrin.h */
+#include <emmintrin.h>
+
 #define DEFAULT_SIZE 512
 
 int main(int argc, char* argv[])
@@ -38,6 +42,19 @@ int main(int argc, char* argv[])
 		fprintf(stderr, "Unable to allocate memory\n");
 		return EXIT_FAILURE;
 	}
-	
+	size_t i, j;
+	puts("a:");
+	for (i = 0; i < n; ++i)
+		for (j = 0; j < n; ++j)
+			scanf("%lf", &amtx[i * n + j]);	
+	puts("b:");
+	for (i = 0; i < n; ++i)
+		for (j = 0; j < n; ++j)
+			scanf("%lf", &bmtx[i * n + j]);
+	dgemm_o3_sse(amtx, bmtx, cmtx, n);
+	puts("\nc:");
+	for (i = 0; i < n; ++i, puts(""))
+		for (j = 0; j < n; ++j)
+			printf("%f\t", cmtx[i * n + j]);
 	return EXIT_SUCCESS;
 }
