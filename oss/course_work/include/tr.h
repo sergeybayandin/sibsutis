2ba775a4cd1a234ee0189aad4e6103d5c2c6fe34
art0fprogramm1ng <serezha-bayandin@mail.ru>
2021-12-06 20:38:40 +0000

    tr: honts type

---
+++ oss/course_work/include/tr.h
@@ -0,0 +1,47 @@
+#ifndef __TR_H__
+#define __TR_H__
+
+#include <bit>
+#include <utility>
+#include <limits>
+
+namespace os
+{
+	
+	namespace details
+	{
+
+		template <class _Tp, std::size_t ...I>
+		constexpr _Tp byteswap_impl(_Tp n, std::index_sequence<I...>) noexcept
+		{
+			constexpr auto digits {
+				std::numeric_limits<unsigned char>::digits
+			};
+			return ((((n >> (I * digits)) & (0xff)) << (sizeof(_Tp) - I - 1) * digits) | ...);
+		}
+
+		template <class _Tp>
+		constexpr _Tp byteswap(_Tp n) noexcept
+		{
+			return 
+				byteswap_impl(
+					n, 
+					std::make_index_sequence<sizeof(_Tp)>{}
+				);
+		}
+
+	} // details
+
+	template <class _Tp>
+	struct htons
+	{
+		static constexpr auto value {
+			std::endian::native == std::endian::big
+			? _Tp::value
+			: details::byteswap(_Tp::value)
+		};
+	};
+
+} // os
+
+#endif // __TR_H__
